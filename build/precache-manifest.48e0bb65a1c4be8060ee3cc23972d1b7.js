a668afa8aba28b3f600c034c98a7949c67416686
Dariusz Rosiek <daro.krk@interia.pl>
2019-02-04 14:02:20 +0100

    gh-pages deploy

---
+++ build/precache-manifest.48e0bb65a1c4be8060ee3cc23972d1b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "60d77e97fdb1bdf0d42e5bee923cce44",
+    "url": "/Weather-API-React/static/media/background.60d77e97.jpg"
+  },
+  {
+    "revision": "3c40ef828b31b2cbc694",
+    "url": "/Weather-API-React/static/js/runtime~main.3c40ef82.js"
+  },
+  {
+    "revision": "c6d14bf729e519da2785",
+    "url": "/Weather-API-React/static/js/main.c6d14bf7.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/Weather-API-React/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "c6d14bf729e519da2785",
+    "url": "/Weather-API-React/static/css/main.b0f68235.chunk.css"
+  },
+  {
+    "revision": "335261e882fafa6c484df571d1ad651f",
+    "url": "/Weather-API-React/index.html"
+  }
+];
